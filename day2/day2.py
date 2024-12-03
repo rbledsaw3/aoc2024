cafa19df9bfe357167bdbe6ca4a15bee5e715149
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-12-02 22:47:29 -0500

    part 2 of day 2 complete

--- day2/day2.py
+++ day2/day2.py
@@ -1,18 +1,25 @@
+def is_safe(line):
+    isIncreasing = line[1] > line[0]
+    for i in range(len(line) - 1):
+        diff = line[i + 1] - line[i]
+        if abs(diff) < 1 or abs(diff) > 3:
+            return False
+        if (diff > 0) != isIncreasing:
+            return False
+    return True
+
+def is_safe_with_dampener(line):
+    for i in range(len(line)):
+        mod_line = line[:i] + line[i + 1:]
+        if len(mod_line) >= 2 and is_safe(mod_line):
+            return True
+    return False
+
 safe = 0
 
 with open("input.txt") as f:
     for l in f:
         line = [int(num) for num in l.split()]
-        isSameDirection = True
-        isIncreasing = line[1] > line[0]
-        for i in range(len(line) - 1):
-            diff = line[i + 1] - line[i]
-            if abs(diff) < 1 or abs(diff) > 3:
-                isSameDirection = False
-                break
-            if (diff > 0) != isIncreasing:
-                isSameDirection = False
-                break
-        if isSameDirection:
+        if is_safe(line) or is_safe_with_dampener(line):
             safe += 1
 print(safe)
