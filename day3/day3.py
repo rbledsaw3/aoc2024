b48ef235ac37b8eb60b99bfa46fb699302223284
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-12-03 23:54:49 -0500

    day 3 part 2 solved

--- day3/day3.py
+++ day3/day3.py
@@ -1,13 +1,23 @@
 import re
 
-pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
-
-total = 0
+all_total = 0
+do_total = 0
+is_doing = True
 
 with open('input.txt', 'r') as f:
-    for line in f:
-        matches = re.findall(pattern, line)
-        for x, y in matches:
-            total += int(x) * int(y)
+    s = f.read().strip()
 
-print(total)
+matches = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", s)
+for m in matches:
+    if m == "do()":
+        is_doing = True
+    elif m == "don't()":
+        is_doing = False
+    else:
+        a, b = re.findall(r"\d+", m)
+        all_total += int(a) * int(b)
+        if is_doing:
+            do_total += int(a) * int(b)
+
+print(f"All total: {all_total}")
+print(f"Do total: {do_total}")
